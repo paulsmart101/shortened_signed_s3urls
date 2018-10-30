650f075fff11db1a0cf2c467cfc86cb861d5b68f
Paul Smart <paulsmart101@gmail.com>
2018-10-31 00:23:04 +0100

    added sns publish

--- make_shortened_signed_urls.py
+++ make_shortened_signed_urls.py
@@ -1,14 +1,13 @@
 #!/bin/python
 import boto3
 import requests
-import os
-import sys
-sys.path.append('../')
+import json
 import bitly_api
 
 # Variables
-access_token = "1234"
-bucket = '<bucket>'
+access_token = "123"
+arn = 'arn:aws:sns:eu-west-2:409201224315:email_paul'
+bucket = 'bucketname'
 keys = ["ACF Presentation Files 1.0.9 BR.zip",
 "ACF Presentation Files 1.0.9 ES.zip",
 "ACF Presentation Files 1.0.11 EN.zip",
@@ -18,6 +17,7 @@ keys = ["ACF Presentation Files 1.0.9 BR.zip",
 
 # Create the service clients
 s3 = boto3.client('s3',region_name='eu-west-2')
+sns = boto3.client('sns',region_name='eu-west-2')
 bitly = bitly_api.Connection(access_token=access_token)
 
 def GenerateUrl(bucket,key):
@@ -35,13 +35,21 @@ def GenerateBitly(longurl,title):
     data = bitly.shorten(longurl)
     assert data is not None
     edit = bitly.user_link_edit(data['url'],edit='title',title=title)
-    return data
+    return data['url']
+
+def SendEmail(message):
+    response = sns.publish(
+        TargetArn=arn,
+        Message=json.dumps({'default': message}),
+        MessageStructure='json'
+    )
+
 
 print "________________________________________________________"
 for key in keys:
     longurl = GenerateUrl(bucket,key)
     shorturl = GenerateBitly(longurl,key)
-    print key
-    print shorturl['url']
+    message = "Key: "+str(key)+"\nLink: "+str(shorturl)
+    SendEmail(message)
+    print message
     print "________________________________________________________"
-
